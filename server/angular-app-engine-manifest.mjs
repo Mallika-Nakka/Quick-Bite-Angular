6beaa15590e56f848335010a8e229d073869a0d3
Mallika Nakka <mallikanakka8@gmail.com>
2026-02-24 06:47:56 +0530

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://Mallika-Nakka.github.io/Quick-Bite-Angular',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
